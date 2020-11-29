eb32ce00886ca253d268b8592d005a8bfecf5806
gbr-sj <46060864+satoshi-2000@users.noreply.github.com>
2020-11-29 09:07:37 +0900

    Add files via upload

---
+++ print.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+
+##### 画面表示 #####
+
+num = 0
+sentence = "Hello World"
+
+#文字列
+print("Hello World!")
+#数値
+print(3)
+#変数
+print(sentence,num)
+
+"""
+Hello World!
+3
+Hello World 0
+"""
