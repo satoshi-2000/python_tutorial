ea92e6644a8ae51a08e7eb0f986b5ee4a99e5125
gbr-sj <46060864+satoshi-2000@users.noreply.github.com>
2020-11-29 09:15:53 +0900

    Add files via upload

---
+++ init.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+#Date : 2020/11/29
+
+##### 入力 #####
+
+num = input()
+print(num) #str型で保存される
+#int型にキャスト
+num2 = int(num)
+print(num2)
+print(num2 + 10)
+
+"""
+"num"
+num2(実際は数値)
+num2 + 10
+"""
